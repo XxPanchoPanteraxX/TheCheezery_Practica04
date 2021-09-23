d417eb49fe4fec8ffe178f8289f67fbaea25deb9
XxPanchoPanteraxX <78341976+XxPanchoPanteraxX@users.noreply.github.com>
2021-09-23 16:57:58 -0700

    Practica 4 terminada

---
+++ app/src/main/java/preciado/francisco/thecheezery_205371/MenuActivity.kt
@@ -0,0 +1,11 @@
+package preciado.francisco.thecheezery_205371
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
